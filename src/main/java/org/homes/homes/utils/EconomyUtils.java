e7afd9b38c8260267cc1b2d7c4e770147dcada70
ComaTV <comatvc@gmail.com>
2025-07-18 00:09:50 +0300

    l

--- src/main/java/org/homes/homes/utils/EconomyUtils.java
+++ src/main/java/org/homes/homes/utils/EconomyUtils.java
@@ -1,6 +1,5 @@
 package org.homes.homes.utils;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Score;
--- src/main/java/org/homes/homes/utils/MessageUtils.java
+++ src/main/java/org/homes/homes/utils/MessageUtils.java
@@ -2,7 +2,6 @@ package org.homes.homes.utils;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.homes.homes.config.ConfigManager;
 
 public class MessageUtils {
